af3dc334438b5c2e6e05ad93500418ebfe514615
Fab <a01367585@itesm.mx>
2022-05-14 18:59:49 -0500

    Iniciado resaltador

--- Resaltador.py
+++ Resaltador.py
@@ -1,2 +1,51 @@
 
+#import re
 
+#------------------------Abre archivo con claves------------------------------
+f = open("lexico_python.txt", "r")
+reserv=f.readline()
+reserv=reserv.strip()
+reserv=reserv.split()
+coment=f.readline()
+opera=f.readline()
+
+reservadas=[]
+reservadas.append(reserv)
+f.close()
+#--------------------------------------------------------------
+
+#------------------------Crea HTML------------------------------
+f = open("salida.html", "w")
+f.write("<!DOCTYPE html>\n")
+f.write("<meta charset='UTF-8'>\n")
+f.write("<title>Salida</title>\n")
+f.write("<link href='estilos.css' rel='stylesheet' type='text/css'>\n")
+f.writelines(["</head>\n", "<body>\n"])
+f.close()
+#--------------------------------------------------------------
+
+#data = input("Escribe el nombre de un archivo de texto Ejemplo: ('archivo.txt'): ")
+data="prueba.py"
+with open(data) as f:
+    contents = f.read()
+    print(contents)
+    s = contents
+    s = s.split("\n")
+    print(s)
+    print("---------------------------------------------------------------------")
+
+Lexema = ""
+token = ""
+p = 0
+
+
+
+
+f = open("salida.html", "w")
+f.write("<p>hola </p>\n")
+f.write("<p class='comentario'> //Prueba con comentario </p>\n")
+
+#---------------Cierra HTML-----------------------------
+f.writelines(["</body>\n</html>"])
+f.close()
+#-------------------------------------------------------
---
+++ prueba.py
@@ -0,0 +1,17 @@
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+
+
+
+my_function("Emil")
+int a = 3;
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus", 1, abc1, abr[1], "")
+
+
+args=21
+21.1
